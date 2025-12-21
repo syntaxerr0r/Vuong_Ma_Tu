00463ff98793af51ef61d031176d6db1a99f4791
Dr.Trune <vodinhtrung.a2@gmail.com>
2025-12-21 12:28:31 +0700

    4.9.2 Fix lỗi swal cho safari

--- auto.js
+++ auto.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name          HH3D - Menu Tùy Chỉnh
 // @namespace     Tampermonkey
-// @version       4.9
+// @version       4.9.2
 // @description   Thêm menu tùy chỉnh với các liên kết hữu ích và các chức năng tự động
 // @author        Dr. Trune
 // @match         https://hoathinh3d.gg/*
+// @require       https://cdn.jsdelivr.net/npm/sweetalert2@11.26.12/dist/sweetalert2.all.min.js
 // @run-at        document-start
 // @grant         GM_xmlhttpRequest
 // @connect       raw.githubusercontent.com
