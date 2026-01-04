0b0e9c0e9be7c7db2dc581e2677350d0749eacdd
Dr.Trune <vodinhtrung.a2@gmail.com>
2026-01-04 20:48:27 +0700

    Update version to 5.3.3 for HH3D - Menu Tùy Chỉnh

--- auto.js
+++ auto.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          HH3D - Menu Tùy Chỉnh
 // @namespace     Tampermonkey
-// @version       5.3.2
+// @version       5.3.3
 // @description   Thêm menu tùy chỉnh với các liên kết hữu ích và các chức năng tự động
 // @author        Dr. Trune
 // @match         https://hoathinh3d.li/*
